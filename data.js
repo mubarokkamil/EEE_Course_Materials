ddada9456b7c19cb3179664edbe5bb43f5d0ccb4
Mubarok Hossen Kamil <mubarokkamil10@gmail.com>
2026-03-19 11:46:44 +0600

    Admin: update course data

--- data.js
+++ data.js
@@ -46,12 +46,6 @@ const DRIVE_DATA = {
       ],
       "files": [
         {
-          "id": "f1773897166576",
-          "name": "Analog to Digital Conversion (ADC)",
-          "type": "pdf",
-          "url": "materials/digital-signal-processing-i/Analog to Digital Conversion.pdf"
-        },
-        {
           "id": "f1773897718885",
           "name": "Convolution",
           "type": "pdf",
